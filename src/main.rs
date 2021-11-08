98147e8860b88446acc134b9672bcd5fc5a72bf1
Jacob Wahlen-Strothman <j.m.wahlenstrothman@gmail.com>
2021-11-07 21:17:35 -0500

    added some comments and minor cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@
 mod hartree_fock;
 
 fn main() {
-	let mut hf_calc = hartree_fock::HFState::init(1000,true,1,true,500,500,4.0,1e-9);
-	hf_calc.mk_guess(0.5);
-	hf_calc.new_den();
-	hf_calc.do_scf(0.05,true);
+
+  // test calculation of a hubbard ring with 1000 sites and 1000 particles with Sz=0 and U/t=4
+
+  let mut hf_calc = hartree_fock::HFState::init(1000,true,1,true,500,500,4.0,1e-9);
+  hf_calc.mk_guess(0.5);
+  hf_calc.new_den();
+  hf_calc.do_scf(0.05,true);
 }
